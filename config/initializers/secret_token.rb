2fb0583faf36fa556707609e3dc05071d2a3fb2a
cesare.diaz <cesare.diaz@gmail.com>
2013-04-01 17:10:38 -0300

    - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedsReader::Application.config.secret_token = '03b12150fe58eaff7548ccb962fabac2ba1cae497d71df398a5e99291ab9198572c20f2a6fece6ee033b8149f229e390bb58b1fbaef75c5284230ef698a24f46'
