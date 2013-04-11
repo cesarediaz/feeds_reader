3ce78eac69b4397a4c6e6674bfbe056f8af5cff9
cesare.diaz <cesare.diaz@gmail.com>
2013-04-11 00:07:17 -0300

    - Spec   * added test for ChannelsUsers model

--- spec/models/channels_users_spec.rb
+++ spec/models/channels_users_spec.rb
@@ -3,13 +3,31 @@ require 'spec_helper'
 describe ChannelsUsers do
 
   before(:each) do
-   @user = FactoryGirl.create(:user)
-   @channel = FactoryGirl.create(:channel, :url => "http://ep00.epimg.net/rss/tags/ultimas_noticias.xml")
-   @user.channels << @channel
+   @user_one = FactoryGirl.create(:user)
+   @user_two = FactoryGirl.create(:user, :email => 'matts@hotmail.com')
+   @channel = FactoryGirl.create(:channel, :url => 'http://ep00.epimg.net/rss/tags/ultimas_noticias.xml')
+   @user_one.channels << @channel
+   @user_two.channels << @channel
   end
 
   it "user should have one channel" do
-    ChannelsUsers.channel_exist_for_user?(@user, @channel).should eql(1)
+    ChannelsUsers.channel_exist_for_user?(@user_one, @channel).should eql(1)
   end
 
+  it "should exist for two users" do
+    ChannelsUsers.channel_counter(@channel.id).should eql(2)
+  end
+
+  it "should delete the channel from the intermediate table for an user" do
+    ChannelsUsers.channel_counter(@channel.id).should eql(2)
+    ChannelsUsers.delete_channel_for_user(@channel.id, @user_one.id)
+    ChannelsUsers.channel_counter(@channel.id).should eql(1)
+  end
+
+  it "should delete the channel from channels table" do
+    ChannelsUsers.channel_counter(@channel.id).should eql(2)
+    ChannelsUsers.delete_channel_for_user(@channel.id, @user_one.id)
+    ChannelsUsers.delete_channel_for_user(@channel.id, @user_two.id)
+    ChannelsUsers.channel_counter(@channel.id).should eql(0)
+  end
 end
