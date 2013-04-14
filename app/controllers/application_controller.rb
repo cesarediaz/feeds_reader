078363bc9813482b4b088680afe5c69789b52e1d
cesare.diaz <cesare.diaz@gmail.com>
2013-04-13 23:27:38 -0300

    - Assets   * added styles for alert
    - Controller
      * added action to control 404 errors
    
    - Layout
      * small modifications to show messages inside the refreshing area of
        layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
 
   before_filter :get_channels
 
+
   def get_channels
     if user_signed_in?
       @channels = current_user.channels
     end
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render_404
+  end
+
+  def render_404
+    flash[:alert] = "404 Not Found!"
+    redirect_to root_path
   end
 
 end
