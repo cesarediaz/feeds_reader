80fdf83b0fae2ed178a54698ce2fd7884d3edd9c
cesare.diaz <cesare.diaz@gmail.com>
2013-04-14 00:04:24 -0300

    - Controller   * refactoring to check if last update of channel was less than 5 minutes     ago and avoid overloading process for server

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+  MINUTES = 5
   def index
     @articles = Article.text_search(params[:query]).page(params[:page])
     respond_to do |format|
@@ -12,19 +13,16 @@ class ArticlesController < ApplicationController
     @channel = Channel.find(params[:channel])
     @starred_articles_ids = current_user.articles.map { |r| r.id }
     begin
+      unless updated_recently?(@channel)
+        Article.update_from_feed(params[:url], params[:channel])
+      end
       params[:page] = params[:page] ||= 1
-      Article.update_from_feed(params[:url], params[:channel])
-      channel = Channel.find(params[:channel])
-      @articles = channel.articles.page(params[:page])
-      respond_to do |format|
-        format.js
-      end
+      @articles = @channel.articles.page(params[:page])
     rescue
-      channel = Channel.find(params[:channel])
-      @articles = channel.articles.page(params[:page])
-      respond_to do |format|
-        format.js
-      end
+      @articles = @channel.articles.page(params[:page])
+    end
+    respond_to do |format|
+      format.js
     end
   end
 
@@ -50,6 +48,12 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       format.js
     end
+  end
+
+  def updated_recently?(channel)
+    hour = Time.now
+    last_update = channel.updated_at
+    hour.min - last_update.min < MINUTES ? true : false
   end
 
 end
