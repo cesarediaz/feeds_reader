2f75520e3960338c845c1f460b93e5887e26c567
cesare.diaz <cesare.diaz@gmail.com>
2013-04-15 00:21:38 -0300

    - Environment  * changes on configuration for delivery email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,6 @@ FeedsReader::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => AppConfig.site['url'] }
+  config.action_mailer.default_url_options = { :host => 'www.leonoticias.com.ar' }
   config.action_mailer.delivery_method = :smtp
 end
