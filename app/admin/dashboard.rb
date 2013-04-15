34b59065f6f25ea02c46591b4742af9b247778ea
cesare.diaz <cesare.diaz@gmail.com>
2013-04-15 02:26:11 -0300

    - ActiveAdmin   * adding dashboard content

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,26 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content :title => proc{ I18n.t("active_admin.dashboard", :locale => :en) } do
+
+    columns do
+
+      column do
+        panel "Recent Users Registered" do
+          table_for  User.limit(15) do
+            column("Login") {|u| status_tag(u.login) }
+            column("Email"){|u| u.email }
+          end
+        end
+      end
+      column do
+        panel "Last Channels" do
+          table_for Channel.order('created_at DESC').limit(15).each do |u|
+            column(:name) {|c| Hpricot.uxs(c.name) }
+            column('Registered at') {|c| c.created_at }
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
